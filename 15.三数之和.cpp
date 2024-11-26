2ae6e7088ae0af869f3f6a32df48e6028526ceb1
qingcity <1249993213@qq.com>
2024-11-26 21:47:17 +0800

    Day21

--- 15.三数之和.cpp
+++ 15.三数之和.cpp
@@ -8,24 +8,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        un
-        unordered_map<int, vector<int>> num;
-
         vector<vector<int>> result;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                num[nums[i] + nums[j]].push_back(i);
-                num[nums[i] + nums[j]].push_back(j);
-            }
-            for (int i = 0; i < nums.size(); i++) {
-                if (num.count(-nums[i]) == 1) {
-                    for (int j = 0; j < num.size(); j+=2) {
-                        if (i != num[-nums[i]][j] && num[-nums[i]][j + 1] != i) {
-                            result.push_back({num[-nums[i]][j], num[-nums[i]][j + 1]});
-                        }
+        sort(nums.begin(), nums.end());
+        int left, right;
+        for (int i = 0; i < nums.size() - 2;) {
+            left = i + 1;
+            right = nums.size() - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[right] == nums[right - 1] ) {
+                        right--;
                     }
+                    right--;    
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    left++;
+                }
+                else if (nums[i] + nums[left] + nums[right] < 0) { // 这里可以不用判断，不过也无所谓
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    left++;
+                }
+                else {
+                    while (left < right &&nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    right--;    
                 }
             }
+            while (i < nums.size() - 2 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+            i++;
         }
         return result;
     }
